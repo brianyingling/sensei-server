ead31c993cde3a37769f9472e1f47a16ce6c806b
Brian Yingling <brian@jet.com>
2019-12-28 00:02:08 -0500

    just reference Query in the resolvers file

--- resolvers/index.js
+++ resolvers/index.js
@@ -1,14 +1,8 @@
 const { filter, find } = require('lodash');
 const books = require('../mocks/books.json');
-const { dashboard } = require('./Query');
+const Query = require('./Query');
 const resolvers = {
-    Query: {
-        author(parent, args, context, info) {
-            return find(authors, { id: args.id });
-        },
-        books: () => books,
-        dashboard
-    },
+    Query,
     Author: {
         books(author) {
             return filter(books, { author: author.name });
